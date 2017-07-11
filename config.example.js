42cf9ce39cc3de907fed4922db1811f19b3e7249
Jake <jake0oo0dev@gmail.com>
2017-07-11 00:27:32 -0500

    :octocat:

---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+	r6stats: {
+		username: 'ItsEpi',
+		password: 'lordchanka',
+		user_agent: 'R6Stats Discord Bot v3'
+	},
+	discord: {
+		token: 'xxxxx'
+	}
+}
---
+++ index.js
@@ -0,0 +1,58 @@
+var Discord = require('discord.js');
+var client = new Discord.Client();
+
+var R6Stats = require('r6stats');
+
+var config = require('./config');
+
+var StatsCommand = require('./lib/commands/stats');
+var OperatorCommand = require('./lib/commands/operator');
+
+
+var r6client = new R6Stats({
+	username: config.r6stats.username,
+	password: config.r6stats.password,
+	user_agent: config.r6stats.user_agent
+});
+
+
+var players = new r6client.services.PlayerService(r6client);
+
+client.on('ready', () => {
+	console.log('Client connected!');
+});
+
+var commands = {
+	'stats': new StatsCommand(players),
+	'operator': new OperatorCommand(players)
+}
+
+client.on('message', message => {
+	var responders = ['!r6s', '!r6stats', '!r6', 'r6s', 'r6stats', 'r6'];
+
+	if (message.author.bot) return;
+
+	var found = false;
+	for (var x of responders) {
+		if (message.content.startsWith(x)) {
+			found = true;
+			break;
+		}
+	}
+
+	if (!found) return;
+
+	var split = message.content.split(' ');
+	var command = split[1].toLowerCase();
+	var args = split.slice(2);
+
+	console.log(command, args);
+
+	if (commands.hasOwnProperty(command)) {
+		var cmd = commands[command];
+
+		cmd.invoke(message, args);
+	}
+});
+
+client.login(config.discord.token);
