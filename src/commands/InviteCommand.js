7d230fcd015b9a8e1a025941be7aeccc06678270
Jake0oo0 <jake0oo0dev@gmail.com>
2018-08-18 16:48:39 -0500

    Add invite command

---
+++ src/commands/InviteCommand.js
@@ -0,0 +1,32 @@
+import BaseCommand from '../BaseCommand'
+
+class InviteCommand extends BaseCommand {
+  constructor ({ }) {
+    super(...arguments)
+  }
+
+  shouldInvoke () {
+    return this._command === 'invite'
+  }
+
+  invoke () {
+    this.reply({
+      embed: {
+        color: 3447003,
+        title: 'R6Stats Bot Invite',
+        description: 'Invite the **R6Stats** bot to your server [here](https://discordapp.com/oauth2/authorize?client_id=334153242152009739&scope=bot&permissions=19456).',
+        thumbnail: {
+          url: 'https://alpha.r6stats.com/img/logos/r6stats-logo-100.png'
+        },
+        footer: {
+          icon_url: 'https://alpha.r6stats.com/img/logos/r6stats-logo-100.png',
+          text: 'Stats Provided by R6Stats.com',
+          url: 'https://r6stats.com'
+        }
+      }
+    })
+  }
+}
+
+
+export default InviteCommand
