7a81f6f054baab9487dd239c3b878d4faa2a2f2a
Liam Potter <me@liam-potter.co.uk>
2019-05-01 14:22:56 +0100

    add missing example config file

---
+++ config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+	r6stats: {
+		login: 'ItsEpi',
+		password: 'lordchanka',
+    user_agent: 'R6Stats Discord Bot v3',
+    base_url: ''
+	},
+	discord: {
+		token: 'xxxxx'
+	}
+}
