5039c07d56bf885d18bd2a3da24a5256ee9bc547
Jake Engel <hi@jengel.me>
2020-03-29 15:15:23 -0500

    feat: update discordjs, add support for custom shard counts/spawns

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js')
+const config = require('./config')
 
-const manager = new Discord.ShardingManager('./bot.js')
+const { shards: shardList, total_shards: totalShards } = config.discord
+
+const manager = new Discord.ShardingManager('./bot.js', { shardList, totalShards })
 
 manager.on('launch', shard => console.log(`- Spawned shard ${shard.id} -`))
 
