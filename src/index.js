f1946461cac5f113d2358cb5adb5c8287b7642b2
Jake Engel <hi@jengel.me>
2020-04-28 21:51:23 -0500

    bugfix: force override manager.spawn()

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js')
 const config = require('../config')
 import * as path from 'path'
 
-const { shards: shardList, total_shards: totalShards } = config.discord
+const { shard_list: shardList, total_shards: totalShards } = config.discord
 
 const manager = new Discord.ShardingManager(path.join(__dirname, 'bot.js'), { shardList, totalShards })
 
 manager.on('shardCreate', shard => console.log(`- Spawned shard ${shard.id} -`))
 
-manager.spawn()
+manager.spawn(totalShards, 5500, -1)
