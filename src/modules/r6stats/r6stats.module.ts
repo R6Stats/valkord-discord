30e44c09e3dca61ce697dd3fe6adab2352aadb58
Jake Engel <jengel@tymail.com>
2020-06-30 12:14:58 -0500

    refactor: change module loading

--- src/modules/r6stats/r6stats.module.ts
+++ src/modules/r6stats/r6stats.module.ts
@@ -1,6 +1,20 @@
-import { Module } from '../../application/modules/module'
+import { ValkordModule } from '../../application/modules'
+import { PingCommand } from './commands/ping.command'
+import { HelpCommand } from './commands/help.command'
+import { InviteCommand } from './commands/invite.command'
+import { StatsCommand } from './commands/stats.command'
+import { OperatorStatsCommand } from './commands/operator-stats.command'
+import { Constructor } from '../../types'
+import { ClientCommand } from '../../application/commands'
 
-@Module({
-  name: 'R6Stats',
-})
-export class R6StatsModule {}
+export class R6StatsModule extends ValkordModule {
+  public getName = (): string => 'R6Stats'
+
+  public getCommands = (): Constructor<ClientCommand>[] => [
+    PingCommand,
+    HelpCommand,
+    InviteCommand,
+    StatsCommand,
+    OperatorStatsCommand,
+  ]
+}
