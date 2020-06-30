e49788f04e16f61a6bc2fa77ee05ad4c1fed6d1c
Jake Engel <jengel@tymail.com>
2020-06-30 10:08:13 -0500

    chore: remove unused files

--- index.js
+++
@@ -1,8 +0,0 @@
-// const Discord = require('discord.js')
-
-// const manager = new Discord.ShardingManager('./bot.js')
-
-// manager.on('launch', shard => console.log(`- Spawned shard ${shard.id} -`))
-
-// manager.spawn(2)
-
